f31d37e820b67d278708b5cf70b3a64b8e9c0bb9
FriendSea <friendsea1995@gmail.com>
2023-08-17 00:53:28 +0900

    sourcegeneratorお試し

---
+++ StateMachineSourceGenerator/StateMachineSourceGenerator/StateMachineSourceGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace StateMachineSourceGenerator
+{
+	[Generator]
+	public class StateMachineSourceGenerator : ISourceGenerator
+	{
+		private const string AttributeText =
+@"
+using System;
+namespace FriendSea.StateMachine
+{
+	[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+	sealed class InjectContextAttribute : Attribute
+	{
+		public InjectContextAttribute()
+		{
+		}
+	}
+}
+";
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+			context.AddSource(
+				hintName: "InjectContextAttribute.g.cs",
+				sourceText: SourceText.From(AttributeText, Encoding.UTF8));
+		}
+
+		public void Initialize(GeneratorInitializationContext context) { }
+	}
+}
