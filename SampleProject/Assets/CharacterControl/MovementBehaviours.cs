53b803c82086f3ec0430229e29ff9fad9c9a8691
FriendSea <friendsea1995@gmail.com>
2023-09-21 19:01:49 +0900

    Add sample

--- SampleProject/Assets/CharacterControl/MovementBehaviours.cs
+++ SampleProject/Assets/CharacterControl/MovementBehaviours.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 using FriendSea.StateMachine;
 using UnityEngine.InputSystem;
 
-class AccelByInput : BehaviourBase
+partial class AccelByInput : BehaviourBase
 {
+	[SerializeField]
+	float factor;
 	[SerializeField]
 	InputActionProperty input;
 	[InjectContext]
 	Movement movement;
 
-	protected override void OnEnter(IContextContainer obj) { }
-	protected override void OnExit(IContextContainer obj) { }
+	protected override void OnEnter(IContextContainer obj) {
+		input.action.Enable();
+	}
+	protected override void OnExit(IContextContainer obj) {
+		input.action.Disable();
+	}
 	protected override void OnUpdate(IContextContainer obj) {
-		movement.Velocity += (Vector3)input.action.ReadValue<Vector2>();
+		var vec = (Vector3)input.action.ReadValue<Vector2>();
+		movement.Velocity += vec * factor;
+	}
+}
+
+partial class MultiplyVelocity : BehaviourBase
+{
+	[SerializeField]
+	float factor;
+	[InjectContext]
+	Movement movement;
+
+	protected override void OnEnter(IContextContainer obj) { }
+
+	protected override void OnExit(IContextContainer obj) { }
+
+	protected override void OnUpdate(IContextContainer obj)
+	{
+		movement.Velocity *= factor;
 	}
 }
