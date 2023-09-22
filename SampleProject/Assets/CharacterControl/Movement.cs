e8b236f9ebfefe534e3d122dc590ec404dc83659
FriendSea <friendsea1995@gmail.com>
2023-09-22 17:50:57 +0900

    Movementがめり込むの修正

--- SampleProject/Assets/CharacterControl/Movement.cs
+++ SampleProject/Assets/CharacterControl/Movement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class Movement : MonoBehaviour
 {
@@ -22,18 +24,23 @@ public class Movement : MonoBehaviour
 		UpdatePosition(Time.deltaTime);
 	}
 
+	RaycastHit[] hits = new RaycastHit[4];
 	void UpdatePosition(float deltaTime)
 	{
 		if (Velocity == Vector3.zero) return;
 		var delta = Velocity * deltaTime;
-		var isHit = Physics.CapsuleCast(GlobalCollisionPoint0 - delta, GlobalCollisionPoint1 - delta, collisionRadius, delta.normalized, out RaycastHit hit, delta.magnitude * 2f);
-		var intersectLength = isHit ?
-			delta.magnitude * 2f - hit.distance :
-			0f;
+		var modDelta = delta;
 
-		delta += hit.normal * Vector3.Dot(hit.normal, -delta.normalized) * intersectLength;
+		var hitcount = Physics.CapsuleCastNonAlloc(GlobalCollisionPoint0 - delta, GlobalCollisionPoint1 - delta, collisionRadius, delta.normalized, hits, delta.magnitude * 2f);
+		for(int i = 0; i < hitcount; i++)
+		{
+			if (Mathf.Approximately(hits[i].distance, 0f)) continue;
+			var intersectLength = delta.magnitude * 2f - hits[i].distance;
+			Debug.Log(hits[i].normal);
+			modDelta += hits[i].normal * Vector3.Dot(hits[i].normal, -delta.normalized) * intersectLength;
+		}
 
-		transform.position += delta;
+		transform.position += modDelta;
 	}
 
 	private void OnDrawGizmos()
