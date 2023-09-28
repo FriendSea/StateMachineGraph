94e3045e06ed50cb7525132fa35b0f3450958e0a
FriendSea <friendsea1995@gmail.com>
2023-09-28 13:57:59 +0900

    Behaviour/Conditionのコピペ実装

--- FriendSea/StateMachine/Editor/StateMachinePropertyDrawer.cs
+++ FriendSea/StateMachine/Editor/StateMachinePropertyDrawer.cs
@@ -47,29 +47,65 @@ namespace FriendSea.StateMachine
 				GUI.Label(dropPos, EditorGUIUtility.IconContent("Warning"));
 				dropPos.x += EditorGUIUtility.singleLineHeight;
 				GUI.Label(dropPos, "<Null>");
+				dropPos.x -= EditorGUIUtility.singleLineHeight;
 			}
 			if (newIndex != currentIndex)
 				property.managedReferenceValue = System.Activator.CreateInstance(types[newIndex]);
 
 			dropPos.x += dropPos.width;
 			dropPos.width = EditorGUIUtility.singleLineHeight;
-			if(property.managedReferenceValue is InjectableObjectBase)
+			if (GUI.Button(dropPos, EditorGUIUtility.IconContent("_Menu"), openScriptStyle))
 			{
-				if (GUI.Button(dropPos, EditorGUIUtility.IconContent("cs Script Icon"), openScriptStyle))
-				{
-					var type = property.managedReferenceValue.GetType();
-					var PathInfo = type.GetField("sourcePathForEditor", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-					var path = PathInfo.GetValue(null) as string;
-					var LineInfo = type.GetField("sourceLineForEditor", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-					var line = (int)LineInfo.GetValue(null);
-					InternalEditorUtility.OpenFileAtLineExternal(path, line);
-				}
+				var menu = new GenericMenu();
+
+				if (property.managedReferenceValue is InjectableObjectBase)
+					menu.AddItem(new GUIContent("Open in Editor"), false, () =>
+					{
+						var type = property.managedReferenceValue.GetType();
+						var PathInfo = type.GetField("sourcePathForEditor", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+						var path = PathInfo.GetValue(null) as string;
+						var LineInfo = type.GetField("sourceLineForEditor", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+						var line = (int)LineInfo.GetValue(null);
+						InternalEditorUtility.OpenFileAtLineExternal(path, line);
+					});
+				else
+					menu.AddDisabledItem(new GUIContent("Open in Editor"));
+
+				if(property.managedReferenceValue != null)
+					menu.AddItem(new GUIContent("Copy"), false, () => {
+						var type = property.managedReferenceValue.GetType();
+						GUIUtility.systemCopyBuffer = $"{type.Assembly.FullName}\n{type.FullName}\n{JsonUtility.ToJson(property.managedReferenceValue)}";
+					});
+				else
+					menu.AddDisabledItem(new GUIContent("Copy"));
+
+				var obj = ParseClipBoard(GUIUtility.systemCopyBuffer);
+				if (obj != null)
+					menu.AddItem(new GUIContent("Paste"), false, () =>
+					{
+						property.serializedObject.Update();
+						property.managedReferenceValue = obj;
+						property.serializedObject.ApplyModifiedProperties();
+					});
+				else
+					menu.AddDisabledItem(new GUIContent("Paste"));
+
+				menu.ShowAsContext();
 			}
 
 			var original = EditorGUIUtility.labelWidth;
 			EditorGUIUtility.labelWidth = 50;
 			EditorGUI.PropertyField(position, property, new GUIContent(""), true);
 			EditorGUIUtility.labelWidth = original;
+		}
+
+		static object ParseClipBoard(string clipBoard)
+		{
+			var lines = clipBoard.Split("\n");
+			if (lines.Length < 3) return null;
+			var type = System.Reflection.Assembly.Load(lines[0])?.GetType(lines[1]);
+			if (type == null) return null;
+			return JsonUtility.FromJson(lines[2], type);
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
