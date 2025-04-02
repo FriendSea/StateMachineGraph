55cac64d53b60644ec1328b6cbaaaa8e55e64b70
friendsea <friendsea1995@gmail.com>
2025-04-02 17:00:19 +0900

    IncrementalGeneratorに変更
    (cherry picked from commit 6cb30666e0a847f6ad7db7ec3ddc90d787cf94fb)

--- StateMachineSourceGenerator/StateMachineSourceGenerator/StateMachineSourceGenerator.cs
+++ StateMachineSourceGenerator/StateMachineSourceGenerator/StateMachineSourceGenerator.cs
@@ -10,109 +10,49 @@ using System.Linq;
 namespace StateMachineSourceGenerator
 {
 	[Generator]
-	public class StateMachineSourceGenerator : ISourceGenerator
+	public class StateMachineSourceGenerator : IIncrementalGenerator
 	{
-		private const string AttributeText = 
-			"""
-			using System;
-			namespace FriendSea.StateMachine {
-				[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-				sealed class InjectContextAttribute : Attribute {
-					public InjectContextAttribute(){}
-				}
-			}
-
-			""";
-
 		static string GetFullName(ISymbol symbol) => string.IsNullOrEmpty(symbol?.ContainingNamespace?.Name) ?
 			symbol.Name :
 			$"{GetFullName(symbol.ContainingNamespace)}.{symbol.Name}";
-
-		static bool IsInjectable(ITypeSymbol symbol)
+		public void Initialize(IncrementalGeneratorInitializationContext context)
 		{
-			if(symbol.BaseType == null) return false;
-			if (GetFullName(symbol.BaseType) == "FriendSea.StateMachine.InjectableObjectBase") return true;
-			return IsInjectable(symbol.BaseType);
-		}
-
-		public void Execute(GeneratorExecutionContext context)
-		{
-			try
+			context.RegisterPostInitializationOutput(static context =>
 			{
-				var classDecs = (context.SyntaxReceiver as SyntexContextReciever).ClassDecs;
-
-				var code = 
-					$$"""
-					/* Auto Generated : {{DateTime.Now}} */
-
-					""";
-				code += AttributeText;
-				foreach(var dec in classDecs)
-				{
-					var sementicModel = context.Compilation.GetSemanticModel(dec.SyntaxTree);
-					var symbol = sementicModel.GetDeclaredSymbol(dec);
-
-					if (!IsInjectable(symbol) || symbol.IsAbstract) continue;
-
-					var fields = new Dictionary<string, string>();
-
-					foreach(var sym in symbol.GetMembers())
-					{
-						var fieldSymbol = sym as IFieldSymbol;
-						if (fieldSymbol == null) continue;
-						if (!fieldSymbol.GetAttributes().Any(s => s.AttributeClass.Name == "InjectContextAttribute")) continue;
-						fields.Add(fieldSymbol.Name, GetFullName(fieldSymbol.Type));
-					}
-
-					var decl = 
-						$$"""
-						partial class {{symbol.Name}} {
-							protected override void OnSetup(FriendSea.StateMachine.IContextContainer ctx) {
-								base.OnSetup(ctx);
-								{{string.Join("\n", fields.Select(s => $"{s.Key} = ctx.Get<{s.Value}>();"))}}
-							}
-
-						#if UNITY_EDITOR
-						#pragma warning disable CS0414
-							static string sourcePathForEditor = @"{{dec.SyntaxTree.FilePath}}";
-							static int sourceLineForEditor = {{dec.GetLocation().GetLineSpan().StartLinePosition.Line}};
-						#endif
+				context.AddSource("InjectContextAttribute.g.cs", """
+					using System;
+					namespace FriendSea.StateMachine {
+						[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+						sealed class InjectContextAttribute : Attribute {
+							public InjectContextAttribute(){}
 						}
-						""";
-					if (!string.IsNullOrEmpty(symbol.ContainingNamespace?.Name))
-						decl = 
-							$$"""
-							namespace {{GetFullName(symbol.ContainingNamespace)}} {
-							{{decl}}
-							}
-							""";
-					code += decl;
+					}
+					""");
+			});
+
+			var provider = context.SyntaxProvider.CreateSyntaxProvider(static (node, token) =>
+			{
+				return node is ClassDeclarationSyntax;
+			},
+			static (context, token) => context);
+
+			context.RegisterSourceOutput(provider, (productionContext, syntaxContext) => {
+				var symbol = Microsoft.CodeAnalysis.CSharp.CSharpExtensions.GetDeclaredSymbol(syntaxContext.SemanticModel, syntaxContext.Node as ClassDeclarationSyntax);
+				if(!symbol.AllInterfaces.Any(i => GetFullName(i) == "FriendSea.StateMachine.IInjectable")) return;
+				var members = symbol.GetMembers().OfType<IFieldSymbol>();
+				if (members.Count() == 0) return;
+				var code = $$"""
+				{{(symbol.ContainingNamespace.IsGlobalNamespace ? "" : $"namespace {GetFullName(symbol.ContainingNamespace)} {{")}}
+				partial class {{symbol.Name}} {
+					public void OnSetup(FriendSea.StateMachine.IContextContainer ctx){
+						base.OnSetup(ctx);
+						{{string.Join("\n", members.Select(s => $"{s.Name} = ctx.Get<{GetFullName(s.Type)}>();"))}}
+					}
 				}
-				context.AddSource(
-					hintName: "InjectContextAttribute.g.cs",
-					sourceText: SourceText.From(code, Encoding.UTF8));
-			}
-			catch(Exception e)
-			{
-				context.AddSource(
-					hintName: "InjectContextAttribute.g.cs",
-					sourceText: SourceText.From(AttributeText + $"/* {e.Message}:{e.StackTrace} */", Encoding.UTF8));
-			}
-		}
-
-		public void Initialize(GeneratorInitializationContext context) {
-			context.RegisterForSyntaxNotifications(new SyntaxReceiverCreator(() => new SyntexContextReciever()));
-		}
-
-		class SyntexContextReciever : ISyntaxReceiver
-		{
-			public List<ClassDeclarationSyntax> ClassDecs { get; } = new List<ClassDeclarationSyntax>();
-
-			public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-			{
-				if (!(syntaxNode is ClassDeclarationSyntax)) return;
-				ClassDecs.Add(syntaxNode as ClassDeclarationSyntax);
-			}
+				{{(symbol.ContainingNamespace.IsGlobalNamespace ? "" : "}")}}
+				""";
+				productionContext.AddSource($"{GetFullName(symbol)}.Inject.g.cs", code);
+            });
 		}
 	}
 }
---
+++ StateMachineSourceGenerator/StateMachineSourceGeneratorTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Hoge.Fuga
+{
+    partial class TestClass : FriendSea.StateMachine.IInjectable
+    {
+        [FriendSea.StateMachine.InjectContext]
+        int hoge;
+
+        [FriendSea.StateMachine.InjectContext]
+        int fuga;
+    }
+}
+
+namespace FriendSea.StateMachine
+{
+    interface IContextContainer { }
+
+    interface IInjectable
+    {
+        void OnSetup(FriendSea.StateMachine.IContextContainer ctx);
+    }
+}
